8bff530e3e0507e0800574f86ac99ae907620bca
purna <purnaraomba@gmail.com>
2024-11-23 19:21:57 +0530

    Roboshop Update

--- golang/main.go
+++ golang/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main(){
-    fmt.Println("Hello, World!")
+import "fmt" // Import the fmt package
+
+func main() {
+    fmt.Println("Hello, World!") // Use fmt to print to the console
 }
